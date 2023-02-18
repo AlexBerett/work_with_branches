dcf3c177d584cb2960e3922da0cd810b614e846b
AlexBerett <berett.89@gmail.com>
2023-02-18 18:03:00 +0300

    Add new function str_func_tittle and docstrings

--- str_func.py
+++ str_func.py
@@ -1,4 +1,5 @@
 def str_func(in_line):
+    # добавил docstring
     """
     Функция принимает на вход строку и возвращает ее со всеми заглавными буквами
     :param in_line: str
@@ -6,3 +7,16 @@ def str_func(in_line):
     """
     result = in_line.upper()
     return result
+
+def str_func_title(in_line):
+    """
+    Функция принимает на вход строку и возвращает каждое слово с заглавными буквами
+    :param in_line: str
+    :return: str with tittle
+    """
+    list_str = in_line.split()
+    result_str = ''
+    for word in list_str:
+        result_str += word.tittle()
+
+    return result_str
