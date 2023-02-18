ba08c0af82bd2c914926d53f492008d0211dfc2d
AlexBerett <125458709+AlexBerett@users.noreply.github.com>
2023-02-18 17:00:59 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
 def str_func(in_line):
     """
-    Функция принимает на вход строку и возвращает ее со всеми заглавными буквами
+    Функция принимает на вход строку и возвращает ее со всеми заглавными буквами.
     :param in_line: str
-    :return: result: str with upper()
+    :return: result: in_line str with upper()
     """
     result = in_line.upper()
     return result
